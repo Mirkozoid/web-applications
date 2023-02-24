3396edf6b0cb31f9f1b0cb18f318ba43e219e013
Mirkozoid <rudanskiu@list.ru>
2023-02-24 13:48:17 +0300

    start

---
+++ Telegram_bot/Telegram_bot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Telegram_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("6104127558:AAF00d6Blwvz4DgCVWzf8usO-xPlR1Ehz2U");
+            botClient.StartReceiving(Update, Error);
+
+            Console.ReadLine();
+        }
+
+        private static Task Update(ITelegramBotClient arg1, Update arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
