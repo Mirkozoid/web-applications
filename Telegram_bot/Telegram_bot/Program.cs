2e4b723a345bc04c7062a75193d88a812280ac28
Mirkozoid <rudanskiu@list.ru>
2023-02-25 10:24:07 +0300

    added an answer

--- Telegram_bot/Telegram_bot/Program.cs
+++ Telegram_bot/Telegram_bot/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Telegram_Bot
 {
@@ -16,14 +18,34 @@ namespace Telegram_Bot
             Console.ReadLine();
         }
 
-        private static Task Update(ITelegramBotClient arg1, Update arg2, CancellationToken arg3)
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
         {
-            throw new NotImplementedException();
+            var message = update.Message;
+            if (message.Text == "/start")
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Hello, I am your financial controller, " +
+                    "every day at 12 o'clock in the afternoon I will send you a little advice on how to dispose of free money." +
+                    " You can also get random advice at any time or offer your own option.");
+                Console.WriteLine($" First Name: {message.ForwardSenderName}\n Chat Id: {message.Chat.Id}\n Message: {message.Text}");
+                //await botClient.SendTextMessageAsync(message.Chat.Id, message.Text, replyMarkup: GetButtons());
+                return;
+            }
         }
 
-        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        //private static IReplyMarkup GetButtons()
+        //{
+        //    return new ReplyKeyboardMarkup
+        //    {
+        //        Keyboard = new List<List<KeyboardButton>>
+        //        {
+        //            new List<KeyboardButton>{new KeyboardButton {Text = "Include tips" }, new KeyboardButton { Text = "No thanks" } }
+        //        }
+        //    };
+        //}
+
+        async static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
         {
-            throw new NotImplementedException();
+          
         }
     }
 }
