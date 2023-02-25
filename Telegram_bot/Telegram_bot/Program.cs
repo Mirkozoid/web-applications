44c209e4542d09131e00a3e212ad3e0cebd7fe55
Mirkozoid <rudanskiu@list.ru>
2023-02-25 12:22:46 +0300

    .

--- Telegram_bot/Telegram_bot/Program.cs
+++ Telegram_bot/Telegram_bot/Program.cs
@@ -20,28 +20,30 @@ namespace Telegram_Bot
         async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
         {
             var message = update.Message;
-            if (message.Text == "/start")
+            if (message.Text != null || message.Text == null)
             {
+              var inlineKeyboard = new InlineKeyboardMarkup(new[]
+              {
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData("Include tips"),
+                    InlineKeyboardButton.WithCallbackData("No thanks")
+                }
+              });
                 await botClient.SendTextMessageAsync(message.Chat.Id, "Hello, I am your financial controller, " +
                     "every day at 12 o'clock in the afternoon I will send you a little advice on how to dispose of free money." +
-                    " You can also get random advice at any time or offer your own option.");
+                    " You can also get random advice at any time or offer your own option.", replyMarkup: inlineKeyboard);
+                Console.WriteLine($" First Name: {message.Chat.FirstName}\n Chat Id: {message.Chat.Id}\n Message: {message.Text}\n at {message.Date}.");
+                
+                Console.WriteLine();           
+                return;
             }
-            await botClient.SendTextMessageAsync(message.Chat.Id, message.Text, replyMarkup: GetButtons());
-            if (message.Text != null)
-            {
-                Console.WriteLine($" First Name: {message.Chat.FirstName}\n Chat Id: {message.Chat.Id}\n Message: {message.Text}");
-                Console.WriteLine();
-            }
+            
         }
-        private static IReplyMarkup GetButtons(List<KeyboardButton>)
+        private async static void BotOnCallBackQueryReceived(ITelegramBotClient botClient, Update inlineKeyboard)
         {
-            return new ReplyKeyboardMarkup
-            {
-                Keyboard = new List<List<KeyboardButton>>
-                {
-                    new List<KeyboardButton>{new KeyboardButton { Text = "Include tips" }, new KeyboardButton { Text = "No thanks" } }
-                }
-            };
+            string selectedOption = inlineKeyboard.CallbackQuery.Data;
+            await botClient.AnswerCallbackQueryAsync(inlineKeyboard.CallbackQuery.Id, $"{selectedOption}");
         }
         async static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
         {
