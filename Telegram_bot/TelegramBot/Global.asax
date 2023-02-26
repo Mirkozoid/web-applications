fed72edb44277585b9514e956f1663b92422bf40
Mirkozoid <rudanskiu@list.ru>
2023-02-26 13:31:34 +0300

    created a selection

--- Telegram_bot/TelegramBot/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="TelegramBot.Global_asax" Language="vb" %>
--- Telegram_bot/Telegram_bot/Program.cs
+++ Telegram_bot/Telegram_bot/Program.cs
@@ -24,20 +24,52 @@ namespace Telegram_Bot
                 case "/start":
                     await botClient.SendTextMessageAsync(message.Chat.Id, "Hello, I am your financial controller, " +
                     "every day at 12 o'clock in the afternoon I will send you a little advice on how to dispose of free money." +
-                    " You can also get random advice at any time or offer your own option.");
+                    " You can also get random advice at any time or offer your own option.", cancellationToken: token) ;
                     ReplyKeyboardMarkup replyKeyboardMarkup = 
                         new(new[]
                         {
-                           new KeyboardButton[] { "Include tips", "No thanks" },
+                           new KeyboardButton[] { "Include tips.", "No thanks." },
                         })
                         {
                         ResizeKeyboard = true
                         };
                     await botClient.SendTextMessageAsync(chatId: message.Chat.Id, text: "Would you like to continue ?",
                     replyMarkup: replyKeyboardMarkup,cancellationToken: token);
-                    Console.WriteLine($" First Name: {message.Chat.FirstName}\n Chat Id: {message.Chat.Id}\n Message: {message.Text}\n at {DateTime.Now}.");
-                Console.WriteLine();                
-                break;
+                    Console.WriteLine($" First Name: {message.Chat.FirstName}.\n Chat Id: {message.Chat.Id}.\n Message: {message.Text}.\n at {DateTime.Now}.");
+                    Console.WriteLine();                
+                    break;
+                case "Include tips.":
+                    ReplyKeyboardMarkup replyKeyboardMarkupforInclude =
+                        new(new[]
+                        {
+                           new KeyboardButton[] { "Get advice.", "Give your advice.","Stop working." },
+                        })
+                        {
+                            ResizeKeyboard = true
+                        };
+                    await botClient.SendTextMessageAsync(chatId: message.Chat.Id, text: "What do you want ?", replyMarkup: replyKeyboardMarkupforInclude,
+                    cancellationToken: token);
+                    break;
+                case "No thanks.":
+                    await botClient.SendTextMessageAsync(chatId: message.Chat.Id, text: "We will be waiting for you, " +
+                    "click /start if you want to start again.", replyMarkup: new ReplyKeyboardRemove(),
+                    cancellationToken: token);                    
+                    Console.WriteLine($" First Name: {message.Chat.FirstName}.\n Chat Id: {message.Chat.Id}.\n Message: {message.Text}.\n at {DateTime.Now}.");
+                    Console.WriteLine();
+                    break;
+                case "Get advice.":
+
+                    break;
+                case "Give your advice.":
+
+                    break;
+                case "Stop working.":
+                    await botClient.SendTextMessageAsync(chatId: message.Chat.Id, text: "We will be waiting for you, " +
+                    "click /start if you want to start again.", replyMarkup: new ReplyKeyboardRemove(),
+                    cancellationToken: token);
+                    Console.WriteLine($" First Name: {message.Chat.FirstName}.\n Chat Id: {message.Chat.Id}.\n Message: {message.Text}.\n at {DateTime.Now}.");
+                    Console.WriteLine();
+                    break;
             }          
         }
         async static Task Error(ITelegramBotClient botClient, Exception exception, CancellationToken token)
