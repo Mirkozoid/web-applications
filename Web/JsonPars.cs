6016d367888b11ab597573823fd5c9a22cfd71e0
Mirkozoid <rudanskiu@list.ru>
2023-05-29 10:14:36 +0300

    add url json

---
+++ Web/JsonPars.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Telegrambot
+{
+    public class ItemJSON
+    {
+        public string urlBackFirst { get; set; }
+        public string urlBackSecond { get; set; }
+        public string urlSiteBananas { get; set; }
+        public string urlSiteLions { get; set; }
+        public string urlSiteFish { get; set; }
+        public string urlSiteFlowers { get; set; }
+        public static string JsonString = File.ReadAllText(@"C:\Users\User\Desktop\Proj\Labs\web-applications\Web\url.json");
+        public static ItemJSON itemJSON = JsonConvert.DeserializeObject<ItemJSON>(JsonString)!;
+    }
+}
